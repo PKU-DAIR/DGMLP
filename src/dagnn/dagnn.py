1c096a38ad47f3e7358f2d92713284aa119ad193
Wentao Zhang <1299799152@qq.com>
2021-08-02 20:30:31 +0800

    Add files via upload

---
+++ src/dagnn/dagnn.py
@@ -0,0 +1,128 @@
+import argparse
+import torch
+from torch.nn import Linear
+import torch.nn.functional as F
+from torch_geometric.nn.conv import MessagePassing, GCNConv
+from torch_scatter import scatter_add
+from torch_geometric.utils import add_remaining_self_loops
+
+from train_eval import *
+from datasets import *
+
+import warnings
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--dataset', type=str, required=True)
+parser.add_argument('--random_splits', type=bool, default=False)
+parser.add_argument('--runs', type=int, default=100)
+parser.add_argument('--epochs', type=int, default=1000)
+parser.add_argument('--lr', type=float, default=0.01)
+parser.add_argument('--weight_decay', type=float, default=0.005)
+parser.add_argument('--early_stopping', type=int, default=100)
+parser.add_argument('--hidden', type=int, default=64)
+parser.add_argument('--dropout', type=float, default=0.5)
+parser.add_argument('--normalize_features', type=bool, default=True)
+parser.add_argument('--K', type=int, default=10)
+
+args = parser.parse_args()
+
+def gcn_norm(edge_index, edge_weight=None, num_nodes=None, improved=False,
+             add_self_loops=True, dtype=None):
+
+    fill_value = 2. if improved else 1.
+    num_nodes = int(edge_index.max()) + 1 if num_nodes is None else num_nodes
+    if edge_weight is None:
+        edge_weight = torch.ones((edge_index.size(1), ), dtype=dtype,
+                                 device=edge_index.device)
+
+    if add_self_loops:
+        edge_index, tmp_edge_weight = add_remaining_self_loops(
+            edge_index, edge_weight, fill_value, num_nodes)
+        assert tmp_edge_weight is not None
+        edge_weight = tmp_edge_weight
+
+    row, col = edge_index[0], edge_index[1]
+    deg = scatter_add(edge_weight, col, dim=0, dim_size=num_nodes)
+    deg_inv_sqrt = deg.pow_(-0.5)
+    deg_inv_sqrt.masked_fill_(deg_inv_sqrt == float('inf'), 0)
+    return edge_index, deg_inv_sqrt[row] * edge_weight * deg_inv_sqrt[col]
+
+class Prop(MessagePassing):
+    def __init__(self, num_classes, K, bias=True, **kwargs):
+        super(Prop, self).__init__(aggr='add', **kwargs)
+        self.K = K
+        self.proj = Linear(num_classes, 1)
+        
+    def forward(self, x, edge_index, edge_weight=None):
+        # edge_index, norm = GCNConv.norm(edge_index, x.size(0), edge_weight, dtype=x.dtype)
+        edge_index, norm = gcn_norm(edge_index, edge_weight, x.size(0), dtype=x.dtype)
+
+
+        preds = []
+        preds.append(x)
+        for k in range(self.K):
+            x = self.propagate(edge_index, x=x, norm=norm)
+            preds.append(x)
+           
+        pps = torch.stack(preds, dim=1)
+        retain_score = self.proj(pps)
+        retain_score = retain_score.squeeze()
+        retain_score = torch.sigmoid(retain_score)
+        retain_score = retain_score.unsqueeze(1)
+        out = torch.matmul(retain_score, pps).squeeze()
+        return out
+    
+    def message(self, x_j, norm):
+        return norm.view(-1, 1) * x_j
+
+    def __repr__(self):
+        return '{}(K={})'.format(self.__class__.__name__, self.K)
+    
+    def reset_parameters(self):
+        self.proj.reset_parameters()
+    
+    
+class Net(torch.nn.Module):
+    def __init__(self, dataset):
+        super(Net, self).__init__()
+        self.lin1 = Linear(dataset.num_features, args.hidden)
+        self.lin2 = Linear(args.hidden, dataset.num_classes)
+        self.prop = Prop(dataset.num_classes, args.K)
+
+    def reset_parameters(self):
+        self.lin1.reset_parameters()
+        self.lin2.reset_parameters()
+        self.prop.reset_parameters()
+
+    def forward(self, data):
+        x, edge_index = data.x, data.edge_index
+        x = F.dropout(x, p=args.dropout, training=self.training)
+        x = F.relu(self.lin1(x))
+        x = F.dropout(x, p=args.dropout, training=self.training)
+        x = self.lin2(x)
+        x = self.prop(x, edge_index)
+        return F.log_softmax(x, dim=1)
+
+warnings.filterwarnings("ignore", category=UserWarning)
+    
+if args.dataset == "Cora" or args.dataset == "CiteSeer" or args.dataset == "PubMed":
+    dataset = get_planetoid_dataset(args.dataset, args.normalize_features)
+    permute_masks = random_planetoid_splits if args.random_splits else None
+    print("Data:", dataset[0])
+    run(dataset, Net(dataset), args.runs, args.epochs, args.lr, args.weight_decay, args.early_stopping, permute_masks, lcc=False)
+elif args.dataset == "cs" or args.dataset == "physics":
+    dataset = get_coauthor_dataset(args.dataset, args.normalize_features)
+    permute_masks = random_coauthor_amazon_splits
+    print("Data:", dataset[0])
+    run(dataset, Net(dataset), args.runs, args.epochs, args.lr, args.weight_decay, args.early_stopping, permute_masks, lcc=False)
+elif args.dataset == "computers" or args.dataset == "photo":
+    dataset = get_amazon_dataset(args.dataset, args.normalize_features)
+    permute_masks = random_coauthor_amazon_splits
+    print("Data:", dataset[0])
+    run(dataset, Net(dataset), args.runs, args.epochs, args.lr, args.weight_decay, args.early_stopping, permute_masks, lcc=True)
+
+
+
+
+
+
